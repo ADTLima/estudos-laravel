2fcf9dd1eec11409f96d9a464cf3b8042b0ca901
Alysson Douglas <alyssonalmeida.adm@gmail.com>
2025-06-21 13:16:27 -0300

    estudo get post

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -6,6 +6,9 @@ class ContatoController extends Controller
 {
     public function Contato()
     {
+        //var_dump($_GET);
+        var_dump($_POST);
+
         return view('site.contato', ['titulo' => 'Contato - teste']);
     }
 }
