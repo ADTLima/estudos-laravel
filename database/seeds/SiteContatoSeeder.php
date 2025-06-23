7e99c1ddadb5aac8e3bc63e30b86083d87d0a450
Alysson Douglas <alyssonalmeida.adm@gmail.com>
2025-06-23 02:13:42 -0300

    finalizado estudos de seeders

---
+++ database/seeds/SiteContatoSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\SiteContato;
+use Illuminate\Database\Seeder; // Import the SiteContato model
+
+class SiteContatoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $contato = new SiteContato();
+        $contato->nome = 'Sistema SG';
+        $contato->telefone = '(85) 99999-9999';
+        $contato->email = 'contaton@sistemasg.com';
+        $contato->motivo_contato = 1;
+        $contato->mensagem = 'Seja Bem Vindo ao Sistema SG!';
+        $contato->save();
+    }
+}
