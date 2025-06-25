a9a092ccbfa6faf76802f9d262f08572ce182ad6
Alysson Douglas <alyssonalmeida.adm@gmail.com>
2025-06-25 20:52:21 -0300

    Estudo sobre como salvar dados no BD

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,13 +3,38 @@
 namespace App\Http\Controllers;
 
 //use GuzzleHttp\Psr7\Request;
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
     public function Contato(Request $request)
     {
-        dd($request);
+        /*
+        echo '<pre>';
+        print_r($request->all());
+        echo '</pre>';
+        echo $request->input('nome');
+        echo '<br>';
+        echo $request->input('email');
+        echo '<br>';
+        */
+
+        /*
+        $contato = new SiteContato();
+        $contato->nome = $request->input('nome');
+        $contato->telefone = $request->input('telefone');
+        $contato->email = $request->input('email');
+        $contato->motivo_contato = $request->input('motivo_contato');
+        $contato->mensagem = $request->input('mensagem');
+
+        //print_r($contato->getAttributes());
+
+        $contato->save();
+        */
+
+        $contato = new SiteContato();
+        $contato->create($request->all());
 
         return view('site.contato', ['titulo' => 'Contato']);
     }
